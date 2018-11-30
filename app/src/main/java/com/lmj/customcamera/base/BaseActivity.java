4b077a5cba4cf5e196f60bad058903464b281ce9
LMJ <1245832008@qq.com>
2018-11-30 13:24:46 +0800

    【修改】 相机页面全屏

--- app/src/main/java/com/lmj/customcamera/base/BaseActivity.java
+++ app/src/main/java/com/lmj/customcamera/base/BaseActivity.java
@@ -1,8 +1,10 @@
 package com.lmj.customcamera.base;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 /**
@@ -10,7 +12,7 @@ import android.view.View;
  *
  * @param <T> 基础Presenter
  */
-public abstract class BaseActivity<T extends BasePresenter> extends Activity {
+public abstract class BaseActivity<T extends BasePresenter> extends AppCompatActivity {
     /** Presenter. */
     protected T mPresenter;
 
@@ -55,6 +57,21 @@ public abstract class BaseActivity<T extends BasePresenter> extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
+        //全屏显示
+        if (Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        } else {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(option);
+        }
     }
 
     @Override
